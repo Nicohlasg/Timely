b8080b209805065f93da8e90c1889e1704a1991a
Nicholas <gggg.24@ichat.sp.edu.sg>
2025-08-22 12:58:49 +0800

    updated sdk version

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -28,8 +28,8 @@ android {
         applicationId = "com.example.calendar_app2"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = 23
-        targetSdk = 35
+        minSdkVersion(flutter.minSdkVersion)
+        targetSdk = 36
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
